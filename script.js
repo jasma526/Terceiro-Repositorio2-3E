05cf9154087537182ffc50bebd5bd3788b086c1c
jasma526 <160799355+jasma526@users.noreply.github.com>
2025-07-01 20:27:46 -0300

    aula 4

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const numeroSenha = document.querySelector('.parametro-senha_texto');
-let tamanhoSenha = 12; 
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
 
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelector('.parametro-senha_botao'); 
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho; 
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     //tamanhoSenha = tamanhoSenha-1;
     if (tamanhoSenha > 1){
         tamanhoSenha--;
-        numeroSenha.textContent = tamanhoSenha;
     }
-
- }
-
-
+    numeroSenha.textContent = tamanhoSenha;
+}
 
 function aumentaTamanho(){
-tamanhoSenha++;
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
